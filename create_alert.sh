6157c487a74a2d5a8a667ffa2fd0dc301a4d355a
chenbishop <chen@twingate.com>
2022-09-28 12:26:30 +0100

    Delete create_alert.sh

--- create_alert.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CONTENT_TYPE="application/json"
-URL="https://events.pagerduty.com/v2/enqueue"
-ROUTING_KEY="xxxxx"
-
-curl -H ${CONTENT_TYPE} -X POST -d '{"payload": {"summary": "Twingate Analytics Error","source": "testsource","severity": "info"},"routing_key": "'${ROUTING_KEY}'","event_action": "trigger"}' ${URL}
